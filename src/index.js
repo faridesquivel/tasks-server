2259a8ae1287623de418c107d2c6a2985e97a17a
Erick Farid <esquivelfarid@gmail.com>
2020-07-19 21:30:28 -0500

    make it cloud

--- src/index.js
+++ src/index.js
@@ -15,8 +15,10 @@ const app = express();
 app.use(cors())
 app.options('*', cors())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 app.use(bodyParser.json());
